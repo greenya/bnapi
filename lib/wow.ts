ac6bf8cec00411cddd6209928d729793cb941aaa
grinevich <yuriy.grinevich@novage.com.ua>
2021-03-31 14:48:26 +0300

    wow: extract basic types: IdName, Media etc.

--- lib/wow.ts
+++ lib/wow.ts
@@ -2,25 +2,34 @@
 
 import { get } from './core.ts'
 
-// Playable Class API
+// Common
 
-interface PlayableClassIndex {
+interface IdName {
     id: number,
     name: string
 }
 
-interface PlayableClass {
-    id: number,
-    name: string,
-    gender_name: { male: string, female: string },
-    power_type: { id: number, name: string },
-    specializations: { id: number, name: string }[],
-    media: { id: number }
+interface GenderName {
+    male: string,
+    female: string
 }
 
-interface PlayableClassMedia {
+interface Media {
     id: number,
-    assets: { key: string, value: string, file_data_id: number }[]
+    assets: {
+        key: string,
+        value: string,
+        file_data_id: number
+    }[]
+}
+
+// Playable Class API
+
+interface PlayableClass extends IdName {
+    gender_name: GenderName,
+    power_type: IdName,
+    specializations: IdName[],
+    media: { id: number }
 }
 
 interface PlayableClassPvpTalentSlot {
@@ -28,7 +37,7 @@ interface PlayableClassPvpTalentSlot {
     unlock_player_level: number
 }
 
-export async function playableClasses(): Promise<PlayableClassIndex[]> {
+export async function playableClasses(): Promise<IdName[]> {
     const { classes } = await get('data/wow/playable-class/index', { namespace: 'static' })
     return classes
 }
@@ -37,7 +46,7 @@ export async function playableClass(id: number): Promise<PlayableClass> {
     return await get(`data/wow/playable-class/${id}`, { namespace: 'static' })
 }
 
-export async function playableClassMedia(id: number): Promise<PlayableClassMedia> {
+export async function playableClassMedia(id: number): Promise<Media> {
     return await get(`data/wow/media/playable-class/${id}`, { namespace: 'static' })
 }
 
@@ -48,21 +57,14 @@ export async function playableClassPvpTalentSlots(id: number): Promise<PlayableC
 
 // Playable Race API
 
-interface PlayableRaceIndex {
-    id: number,
-    name: string
-}
-
-interface PlayableRace {
-    id: number,
-    name: string,
-    gender_name: { male: string, female: string },
+interface PlayableRace extends IdName {
+    gender_name: GenderName,
     faction: { type: string, name: string },
     is_selectable: boolean,
     is_allied_race: boolean
 }
 
-export async function playableRaces(): Promise<PlayableRaceIndex[]> {
+export async function playableRaces(): Promise<IdName[]> {
     const { races } = await get('data/wow/playable-race/index', { namespace: 'static' })
     return races
 }

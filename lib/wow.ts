33ddff769852f40a2e723b0da30904ec2691bba6
grinevich <yuriy.grinevich@novage.com.ua>
2021-03-31 14:53:36 +0300

    wow: Playable Specialization API

--- lib/wow.ts
+++ lib/wow.ts
@@ -72,3 +72,54 @@ export async function playableRaces(): Promise<IdName[]> {
 export async function playableRace(id: number): Promise<PlayableRace> {
     return await get(`data/wow/playable-race/${id}`, { namespace: 'static' })
 }
+
+// Playable Specialization API
+
+interface IdNameOpt {
+    id: number,
+    name?: string
+}
+
+interface SpellTooltip {
+    description: string,
+    cast_time: string,
+    power_cost?: string,
+    cooldown?: string
+}
+
+interface PlayableSpecializationIndex {
+    character_specializations: IdName[],
+    pet_specializations: IdName[]
+}
+
+interface PlayableSpecialization extends IdName {
+    role: { type: string, name: string },
+    playable_class: IdNameOpt,
+    gender_description: GenderName,
+    talent_tiers?: {
+        level: number,
+        talents: {
+            talent: IdName,
+            spell_tooltip: SpellTooltip,
+            column_index: number
+        }[],
+        tier_index: number
+    }[],
+    pvp_talents?: {
+        talent: IdName,
+        spell_tooltip: SpellTooltip
+    }[],
+    media: { id: number }
+}
+
+export async function playableSpecializations(): Promise<PlayableSpecializationIndex> {
+    return await get('data/wow/playable-specialization/index', { namespace: 'static' })
+}
+
+export async function playableSpecialization(id: number): Promise<PlayableSpecialization> {
+    return await get(`data/wow/playable-specialization/${id}`, { namespace: 'static' })
+}
+
+export async function playableSpecializationMedia(id: number): Promise<Media> {
+    return await get(`data/wow/media/playable-specialization/${id}`, { namespace: 'static' })
+}
